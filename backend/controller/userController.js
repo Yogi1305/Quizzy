20b4098c441e18a192b522b7b80385b15d64a720
Yogi1305 <yogesh.22273@knit.ac.in>
2025-08-11 16:32:12 +0530

    push notication setup

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,10 +3,24 @@ import { User } from "../model/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import admin from "firebase-admin";
-import {serviceAccount} from "./../serviceAccountKey.js" ;
+// import {serviceAccount} from "./../serviceAccountKey.js" ;
+import dotenv from "dotenv";
+dotenv.config();
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // databaseURL: "https://your-database-name.firebaseio.com" // Replace with your Firebase database URL
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN
+  })
 });
 
 
